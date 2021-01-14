bdc74684696a96ea5b96a5218742902280e87549
fatima.yamina.salem <fatima.yamina.salem@gmail.com>
2021-01-14 14:24:05 +0100

    Adding Classes

---
+++ src/CommandFactory.java
@@ -0,0 +1,4 @@
+public class CommandFactory {
+    public void GetCommands() {
+    }
+}
---
+++ src/ShellCommand.java
@@ -0,0 +1,9 @@
+import java.util.List;
+public abstract class ShellCommand {
+    protected List<String> parameters;
+    public void setParameters(List<String> parameters) {
+        this.parameters = parameters;
+    }
+    public void Execute(){
+    }
+}
