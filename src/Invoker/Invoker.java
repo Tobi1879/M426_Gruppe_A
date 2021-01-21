7ec44d3fc220f585aad1aa049b1dc1250816cbd8
fatima.yamina.salem <fatima.yamina.salem@gmail.com>
2021-01-21 15:14:00 +0100

    Exit adding method

--- src/Invoker/Invoker.java
+++ src/Invoker/Invoker.java
@@ -4,6 +4,8 @@ import Commands.ShellCommand;
 
 public class Invoker {
 
+    //Nimmt die Command-Eingaben der Console entgegen und leitet sie weiter zum Parser
+
     public void ExecuteCommand(){
 
     }
