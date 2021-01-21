e3de8ea8a5f788b3c81eb55a636b31cf6b120f56
Tobias <tobi1879@gmail.com>
2021-01-21 15:05:59 +0100

    Console weitergemacht

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import Commands.ExitCommand;
+import Console.Console;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf(System.getProperty("os.name"));
 
-
+        Console console = new Console();
+        console.Start();
     }
 }
