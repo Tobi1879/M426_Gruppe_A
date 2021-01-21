a344649daa742ee0381130f4b6bd607eb626b6a4
Christian Scherrer <christian.scherrer@bwz-rappi.ch>
2021-01-21 15:44:02 +0100

    IDrive filled with methods

--- src/FileSystem/IDrive.java
+++ src/FileSystem/IDrive.java
@@ -1,4 +1,14 @@
 package FileSystem;
 
 public interface IDrive {
+    public String name = null;
+    public String label = null;
+    public int size = 0;
+    public Directory rootDirectory = null;
+    public Directory currentDirecotry = null;
+    public String prompt = null;
+
+    public void setCurrentDirectory(Directory directory);
+    public void restore();
+    public void Save();
 }
