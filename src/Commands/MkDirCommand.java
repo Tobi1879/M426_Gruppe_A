bf54b35e92560f67a0934c7d376d4a8bb94ba433
maxgr <maximilian.gruter@bwz-rappi.ch>
2021-01-28 14:20:30 +0100

    MkDirCommand anfang? weiss ned wo igab stattfindet

--- src/Commands/MkDirCommand.java
+++ src/Commands/MkDirCommand.java
@@ -1,6 +1,13 @@
 package Commands;
 
+import FileSystem.Directory;
+import Invoker.IOutputWriter;
+
 public class MkDirCommand extends ShellCommand {
 
+    @Override
+    public void Execute(IOutputWriter iOutputWriter) {
 
+        Directory directory = new Directory();
+    }
 }
