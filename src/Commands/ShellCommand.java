30c05d3731ddbcd0a097a26083eba828d62de1a4
fatima.yamina.salem <fatima.yamina.salem@gmail.com>
2021-02-23 11:45:39 +0100

    create command

--- src/Commands/ShellCommand.java
+++ src/Commands/ShellCommand.java
@@ -2,12 +2,14 @@ package Commands;
 
 import Invoker.IOutputWriter;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 public abstract class ShellCommand {
     protected List<String> parameters;
     public void setParameters(List<String> parameters) {
         this.parameters = parameters;
     }
-    public void Execute(IOutputWriter iOutputWriter){
+    public void Execute(IOutputWriter iOutputWriter) throws IOException {
     }
 }
