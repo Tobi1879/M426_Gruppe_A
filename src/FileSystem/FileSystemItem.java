61b38fb62e961a695f50f7051919530311721ece
MihajloZivanovic <mih.zivanov199@hotmail.com>
2021-02-23 10:46:37 +0100

    FileSystemFile Getter und Setter erstellt.

--- src/FileSystem/FileSystemItem.java
+++ src/FileSystem/FileSystemItem.java
@@ -4,4 +4,28 @@ public abstract class FileSystemItem {
     private Directory parentDirectory;
     public String name;
     public String path;
+
+    public Directory getParentDirectory() {
+        return parentDirectory;
+    }
+
+    public void setParentDirectory(Directory parentDirectory) {
+        this.parentDirectory = parentDirectory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
