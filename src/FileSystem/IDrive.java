06551a9a0b70ecec7151afb7afde85e74f7e1bb9
fatima.yamina.salem <fatima.yamina.salem@gmail.com>
2021-02-23 10:35:23 +0100

    IDrive to abstract

--- src/FileSystem/IDrive.java
+++ src/FileSystem/IDrive.java
@@ -1,14 +1,22 @@
 package FileSystem;
 
-public interface IDrive {
-    public String name = null;
-    public String label = null;
-    public int size = 0;
-    public Directory rootDirectory = null;
-    public Directory currentDirecotry = null;
-    public String prompt = null;
+public abstract class IDrive {
+    public  String name = null;
+    public  String label = null;
+    public  int size = 0;
+    public  Directory rootDirectory = null;
+    public  Directory currentDirecotry = null;
+    public  String prompt = null;
 
-    public void setCurrentDirectory(Directory directory);
-    public void restore();
-    public void Save();
+    public  void setCurrentDirectory(Directory directory) {
+
+    }
+
+    public  void restore() {
+
+    }
+
+    public  void Save() {
+
+    }
 }
