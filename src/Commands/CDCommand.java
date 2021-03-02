30f2adac3aa73f8e31bd9d80fb399b884c35244a
maxgr <maximilian.gruter@bwz-rappi.ch>
2021-03-02 09:41:07 +0100

    cd command oder so

--- src/Commands/CDCommand.java
+++ src/Commands/CDCommand.java
@@ -2,17 +2,39 @@ package Commands;
 
 import FileSystem.Directory;
 import FileSystem.Drive;
+
+import FileSystem.FileSystemItem;
 import Invoker.IOutputWriter;
 
-public class CDCommand extends ShellCommand{
+import java.util.List;
+
+public class CDCommand extends ShellCommand {
 
 
     @Override
     public void Execute(IOutputWriter iOutputWriter) {
 
         String destination = this.parameters.get(0);
+        Boolean isDirectory = false;
+        List<FileSystemItem> fileSystemItems = Drive.getFileSystemItems();
+
+        switch (destination){
+            case "..":
+                ;
+            case "\\":
+                ;
+            default:
+                for (FileSystemItem fileSystemItem : fileSystemItems) {
+                    if (fileSystemItem.getParentDirectory() == Drive.getCurrentDirecotry()){
+                        // name überprüefe und wenner glich isch denn das directory als currentdirectory neh
+                        // isDirectory uf true setze, zum nocher überprüefe ob de Fehlermeldig musch usgeh
+                    }
+                }
+                ;
+        }
 
         Directory directory = new Directory();
+
 
         Drive.setCurrentDirecotry(directory);
 
