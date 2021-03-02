3bd45166d38d5cbe6ef4a6a337e7f126b6606b1f
Tobias <tobi1879@gmail.com>
2021-03-02 09:30:52 +0100

    Del Command Anpassungen

--- src/Commands/DelCommand.java
+++ src/Commands/DelCommand.java
@@ -1,18 +1,33 @@
 package Commands;
 
+import FileSystem.Drive;
 import FileSystem.File;
+import FileSystem.FileSystemItem;
 import Invoker.ConsoleOutputWriter;
 import Invoker.IOutputWriter;
 
 import java.io.IOException;
+import java.util.List;
 
 public class DelCommand extends ShellCommand{
     @Override
     public void Execute(IOutputWriter iOutputWriter) throws IOException {
-        File file = new File();
         String selectedFile = this.parameters.get(0);
-        if(file.getName().equals(selectedFile)){
-            file = null;
+
+        Boolean isFile = false;
+
+        List<FileSystemItem> fileSystemItems = Drive.getFileSystemItems();
+        for (FileSystemItem fileSystemItem : fileSystemItems) {
+            if (fileSystemItem.getParentDirectory() == Drive.getCurrentDirecotry()){
+                if (fileSystemItem.getName() == selectedFile) {
+                    isFile = true;
+                    fileSystemItems.remove(fileSystemItem);
+                }
+            }
+        }
+
+        if(isFile){
+            Drive.setFileSystemItems(fileSystemItems);
         }
         else{
             iOutputWriter.PrintLine("File does not exist.");
