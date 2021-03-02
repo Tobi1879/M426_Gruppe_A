8cb867f39bad3f8e1bbcfe4ee1e489c0c88a46b7
Tobias <tobi1879@gmail.com>
2021-03-02 09:25:20 +0100

    Dir + mkdir Command Anpassungen

--- src/Commands/MkDirCommand.java
+++ src/Commands/MkDirCommand.java
@@ -5,6 +5,7 @@ import FileSystem.Drive;
 import FileSystem.FileSystemItem;
 import Invoker.IOutputWriter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MkDirCommand extends ShellCommand {
@@ -13,14 +14,36 @@ public class MkDirCommand extends ShellCommand {
     public void Execute(IOutputWriter iOutputWriter) {
 
         String name = this.parameters.get(0);
-        String path = Drive.getPrompt();
-        Directory directory = new Directory();
-        //test test test test test test test
-        directory.setName(name);
-        directory.setPath(path);
-        directory.setParentDirectory(Drive.getCurrentDirecotry());
+
+        //name überprüfen
         List<FileSystemItem> fileSystemItems = Drive.getFileSystemItems();
-        fileSystemItems.add(directory);
-        Drive.setFileSystemItems(fileSystemItems);
+
+        Boolean nameIsOk = true;
+
+        for (FileSystemItem fileSystemItem : fileSystemItems) {
+            if (fileSystemItem.getParentDirectory() == Drive.getCurrentDirecotry()){
+                if (fileSystemItem.getName() == name) {
+                    nameIsOk = false;
+                }
+            }
+        }
+
+        // Spezialfälle
+        if (name.contains(".")) {
+            iOutputWriter.PrintLine("ungültiger Verzeichnisname!");
+        } else if (nameIsOk) {
+            String path = Drive.getPrompt();
+            Directory directory = new Directory();
+            //test test test test test test test
+            directory.setName(name);
+            directory.setPath(path);
+            directory.setParentDirectory(Drive.getCurrentDirecotry());
+
+            // zum FileSystemItemList hinzufügen
+            fileSystemItems.add(directory);
+            Drive.setFileSystemItems(fileSystemItems);
+        } else {
+            iOutputWriter.PrintLine("Verzeichnis existiert bereits!");
+        }
     }
 }
